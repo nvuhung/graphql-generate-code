cfb4c6658d394b0a44ce0ca5f717e084fadcb904
Hung Nguyen Vu <nvuhung@outlook.com>
2019-02-19 21:56:46 +0700

    Updates

---
+++ precache-manifest.8100c0c8526f63c198de46e7eaf105a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fcf957a295656fd0101",
+    "url": "/nvuhung/graphql-generate-code/static/js/runtime~main.6fcf957a.js"
+  },
+  {
+    "revision": "a14380c8bbfbbdf2157a",
+    "url": "/nvuhung/graphql-generate-code/static/js/main.a14380c8.chunk.js"
+  },
+  {
+    "revision": "a073345590d93440c7ba",
+    "url": "/nvuhung/graphql-generate-code/static/js/2.a0733455.chunk.js"
+  },
+  {
+    "revision": "a14380c8bbfbbdf2157a",
+    "url": "/nvuhung/graphql-generate-code/static/css/main.a87d520f.chunk.css"
+  },
+  {
+    "revision": "a073345590d93440c7ba",
+    "url": "/nvuhung/graphql-generate-code/static/css/2.1461430b.chunk.css"
+  },
+  {
+    "revision": "d09f61a0bcaf80356251585089930ac4",
+    "url": "/nvuhung/graphql-generate-code/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/nvuhung/graphql-generate-code/precache-manifest.8100c0c8526f63c198de46e7eaf105a0.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/nvuhung/graphql-generate-code/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
